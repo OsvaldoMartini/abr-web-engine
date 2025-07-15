d5eec7c824235e0d6f903f383942a95b929a7fb4
Osvaldo Martini <osvaldo.martini@gmail.com>
2025-07-15 12:14:00 +0200

    GET DATA FOR THE VERSION

--- src/main/java/com/allinweb/ch/Engine.java
+++ src/main/java/com/allinweb/ch/Engine.java
@@ -482,6 +482,7 @@ public class Engine {
 
                         if (blockActive) {
                             excelFieldName = blockLoad.getExportFile();
+
                             if (!Strings.isNullOrEmpty(excelFieldName)) {
                                 String[] parts = excelFieldName.split(":");
                                 if (parts.length > 2) {
