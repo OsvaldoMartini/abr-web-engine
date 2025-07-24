3c309997d37fd72d02c8ae46e0e4e0a10f50a151
Osvaldo Martini <osvaldo.martini@gmail.com>
2025-07-24 18:19:46 +0200

    COMMENTED DB SETUP WARNING

--- src/main/java/com/allinweb/ch/util/ARPropertyManager.java
+++ src/main/java/com/allinweb/ch/util/ARPropertyManager.java
@@ -131,7 +131,7 @@ public class ARPropertyManager {
 
             missingMandatoryPats();
 
-            setProperty(ARPropertyEnum.VERSION.getValue(), "AR Web v4.1f Beta Test");
+            setProperty(ARPropertyEnum.VERSION.getValue(), "AR Web v4.6f Beta Test");
             setProperty(ARPropertyEnum.BUILD.getValue(), "Build: 23/07/2025");
 
             //        } catch (Exception e) {
@@ -242,9 +242,9 @@ public class ARPropertyManager {
     public List<String> checkProperties(Properties properties) {
         String[] requiredProperties = {
             "data_base",
-            "db_url",
-            "db_user",
-            "db_pwd",
+            //            "db_url",
+            //            "db_user",
+            //            "db_pwd",
             "path_excel",
             "path_log",
             "path_db",
